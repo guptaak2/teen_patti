9a32109c1c14bba4e389d57b28564936a8696694
Akhil Gupta <akhil.1996@live.com>
2020-04-12 14:14:05 -0400

    Updates

---
+++ precache-manifest.7cbb2ae347c28abec6c3c247a5b35088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d991ac2c67d03f750be6b0e5a2c968",
+    "url": "/teen_patti/index.html"
+  },
+  {
+    "revision": "a673dd225048c2943c0b",
+    "url": "/teen_patti/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d4b1ffe892ff5694421",
+    "url": "/teen_patti/static/js/2.53eb6071.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/teen_patti/static/js/2.53eb6071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a673dd225048c2943c0b",
+    "url": "/teen_patti/static/js/main.f978ec89.chunk.js"
+  },
+  {
+    "revision": "bba76384757b3d75c3b8",
+    "url": "/teen_patti/static/js/runtime-main.78c698ac.js"
+  }
+]);
